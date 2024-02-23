2c5e18c15ad84bd7876ebff8827c5d4c5850338c
Ruud Andriessen <ruud.andriessen@uipath.com>
2024-02-23 15:56:05 +0100

    chore: migrate to correct user entity name

---
+++ Todo.SqlServer/Migrations/20240223145532_UserEntityRename.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserEntityRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoEntity_UserEntity_UserId",
+                table: "TodoEntity");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserEntity",
+                table: "UserEntity");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TodoEntity",
+                table: "TodoEntity");
+
+            migrationBuilder.RenameTable(
+                name: "UserEntity",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "TodoEntity",
+                newName: "Todos");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TodoEntity_UserId",
+                table: "Todos",
+                newName: "IX_Todos_UserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Todos",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Todos",
+                table: "Todos",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_Users_UserId",
+                table: "Todos",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_Users_UserId",
+                table: "Todos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Todos",
+                table: "Todos");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "UserEntity");
+
+            migrationBuilder.RenameTable(
+                name: "Todos",
+                newName: "TodoEntity");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Todos_UserId",
+                table: "TodoEntity",
+                newName: "IX_TodoEntity_UserId");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "UserEntity",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "TodoEntity",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserEntity",
+                table: "UserEntity",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TodoEntity",
+                table: "TodoEntity",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoEntity_UserEntity_UserId",
+                table: "TodoEntity",
+                column: "UserId",
+                principalTable: "UserEntity",
+                principalColumn: "Id");
+        }
+    }
+}
